104a7b94a48a1348ab41c6aa0a1aa4e5953a72e7
Enzo Romero <enzoromerov@hotmail.com>
2021-05-26 22:29:24 -0300

    first commit

---
+++ app/controllers/v1/game_controller.rb
@@ -0,0 +1,48 @@
+require 'json'
+
+class V1::GameController < ApplicationController
+
+  def index
+    render(json: Game.all, status: 200)
+  end
+
+  def create
+    game = Game.new({player_id1:"1",player_id2:"2",board:[["","",""],["","",""],["","",""]],  moveNumber: 0})
+    if game.save
+      render(json: { game: game }, status: 200)
+    else
+        render(json: format_error(request.path, game.errors.full_messages), status: 401)
+    end
+  end
+
+  def destroy
+    game = Game.find_by(id: params[:id])
+    if game.delete
+        render(json: { game: game }, status: 200)
+      else
+          render(json: format_error(request.path, game.errors.full_messages), status: 401)
+      end
+    end
+
+
+    def newmovement
+        game =  Game.find_by(id: params[:gameId])
+        currentPlayer = params[:playerId]
+        x = params[:movement][1]
+        y = params[:movement][2]
+        board = JSON[game.board]
+        isValidMovement = game.isValidMovement?(board, x,y, game.result)
+        if(isValidMovement)
+            board[x][y] = currentPlayer
+            game.board = board
+            game.moveNumber += 1
+            game.result = game.get_result(board, game.moveNumber)
+            if game.save
+                return render(json: {board: board, winner: game.result }, status: 200)
+            end
+            
+        end
+        render(json: {message: "Movimiento inválido vuelva a intentarlo" }, status: 200)
+    end
+
+end
